f73b0aed87cac6b7ab15ebfbf8ca28ee076abd63
emptyset <emptierset@gmail.com>
2024-07-11 15:45:42 -0700

    Only watch .yml files in prepare.js
    Without this, prepare.js crashes when editing .yml files in Vim, because
    it also watches Vim's swap files and tries to parse them as yaml.

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,8 +1,9 @@
 
+const path = require('path');
 const yaml = require('js-yaml');
 const fs = require('fs');
 
-const ruleFiles = fs.readdirSync('src/assets/i18n/rules');
+const ruleFiles = fs.readdirSync('src/assets/i18n/rules').filter(x => path.extname(x) === '.yml');
 ruleFiles.forEach(file => {
 
   const fileL = fs.readFileSync(`src/assets/i18n/rules/${file}`, 'UTF-8');
