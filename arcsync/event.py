a7f8aa1f1491184b26ca4271d538d101bbfe2d23
emptyset <emptierset@gmail.com>
2024-09-20 17:10:00 -0700

    Use TYPE_CHECKING only imports in event.py

--- arcsync/event.py
+++ arcsync/event.py
@@ -1,9 +1,13 @@
 import abc
 import dataclasses
+import typing
 
-import arcsync.ambition
-from arcsync.color import Color
-from arcsync.piece import DamageablePiece
+# This file will need to import most of the game's concepts, but only as type annotations. We'll
+# import them as TYPE_CHECKING only to avoid circular imports.
+if typing.TYPE_CHECKING:
+    from arcsync.ambition import Ambition
+    from arcsync.color import Color
+    from arcsync.piece import DamageablePiece
 
 
 @dataclasses.dataclass(frozen=True)
@@ -13,19 +17,19 @@ class Event(object, metaclass=abc.ABCMeta):
 
 @dataclasses.dataclass(frozen=True)
 class AmbitionDeclaredEvent(Event):
-    player: Color
-    ambition: arcsync.ambition.Ambition
+    player: "Color"
+    ambition: "Ambition"
 
 
 @dataclasses.dataclass(frozen=True)
 class InitiativeSeizedEvent(Event):
-    player: Color
+    player: "Color"
 
 
 @dataclasses.dataclass(frozen=True)
 class PlayerDestroyedPieceEvent(Event):
-    player: Color
-    piece: DamageablePiece
+    player: "Color"
+    piece: "DamageablePiece"
 
 
 if __name__ == "__main__":
