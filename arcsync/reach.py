0ece1362e2e0252a469ff0351b9f60f0bb5760da
emptyset <emptierset@gmail.com>
2024-09-20 17:29:55 -0700

    Construct Systems using SystemIDs now
    Also:
    - Demote SystemID from NewType to alias because it's obvious what a
      SystemID looks like, and I was getting tired of writing it.
    - Allow "correct" and "convenient" SystemIDs: "1.3" and "1H"
      respectively, for example.

--- arcsync/reach.py
+++ arcsync/reach.py
@@ -1,5 +1,6 @@
 import abc
 import enum
+import re
 import typing
 from collections.abc import Collection, Mapping, MutableMapping, MutableSet
 from typing import Final, NewType
@@ -22,9 +23,7 @@ class PlanetSymbol(enum.Enum):
     CRESCENT = enum.auto()
     HEXAGON = enum.auto()
 
-    # TODO(base): Make aliases for these, maybe using numbers for one set of aliases and pretty
-    # unicode for another.
-    def abbreviation(self) -> str:
+    def alphabetic_abbreviation(self) -> str:
         match self:
             case PlanetSymbol.ARROW:
                 return "A"
@@ -33,14 +32,51 @@ class PlanetSymbol(enum.Enum):
             case PlanetSymbol.HEXAGON:
                 return "H"
 
+    def numeric_abbreviation(self) -> str:
+        match self:
+            case PlanetSymbol.ARROW:
+                return "1"
+            case PlanetSymbol.CRESCENT:
+                return "2"
+            case PlanetSymbol.HEXAGON:
+                return "3"
+
+    @staticmethod
+    def from_string(s: str) -> "PlanetSymbol":
+        match s:
+            case "A" | "1":
+                return PlanetSymbol.ARROW
+            case "C" | "2":
+                return PlanetSymbol.CRESCENT
+            case "H" | "3":
+                return PlanetSymbol.HEXAGON
+        raise ValueError(f"Input '{s}' could not be converted to PlanetSymbol.")
+
 
 Cluster = NewType("Cluster", int)
 
-SystemID = NewType("SystemID", str)
+SystemID = str
+
+
+def _decompose_system_id(system_id: SystemID) -> tuple[Cluster, PlanetSymbol | None]:
+    m = re.match(r"^([1-6])([ACHG])$", system_id)
+    if m is None:
+        m = re.match(r"^([1-6])\.([1-3G])$", system_id)
+        if m is None:
+            raise ValueError(f"Input '{system_id}' did not match any known SystemID regex.")
+
+    cluster = int(m.group(1))
+    planet_or_gate_string = m.group(2)
+
+    planet_symbol: PlanetSymbol | None = None
+    if planet_or_gate_string != "G":
+        planet_symbol = PlanetSymbol.from_string(planet_or_gate_string)
+
+    return Cluster(cluster), planet_symbol
 
 
 class System(object, metaclass=abc.ABCMeta):
-    # Conceptually final; can't figure out how to make mypy happy, though.
+    # cluster is conceptually final; can't figure out how to make mypy happy, though.
     cluster: Cluster
 
     adjacencies: Collection["System"]
@@ -58,18 +94,21 @@ class System(object, metaclass=abc.ABCMeta):
 class Gate(System):
     cluster: Cluster
 
-    adjacencies: Collection[System]
+    adjacencies: MutableSet[SystemID]
     pieces: MutableSet[Piece]
 
-    def __init__(self, cluster: Cluster) -> None:
+    def __init__(
+        self,
+        cluster: Cluster,
+    ) -> None:
         self.cluster = cluster
+        self.adjacencies = set()
 
         self.pieces = set()
-        self.adjacencies = []
 
     @property
     def id(self) -> SystemID:
-        return SystemID(f"{self.cluster}G")
+        return f"{self.cluster}G"
 
 
 @typing.final
@@ -79,29 +118,32 @@ class Planet(System):
     type: Final[PlanetType]
     num_slots: int
 
-    adjacencies: Collection[System]
+    adjacencies: MutableSet[SystemID]
     pieces: MutableSet[Piece]
 
     # TODO(base): Move all @propertys to before __init__, so they are more like normal attributes.
     @property
     def id(self) -> SystemID:
-        return SystemID(f"{self.cluster}{self.symbol.abbreviation()}")
+        return f"{self.cluster}{self.symbol.alphabetic_abbreviation()}"
 
     def __init__(
         self,
-        cluster: Cluster,
-        planet_symbol: PlanetSymbol,
+        system_id: SystemID,
         planet_type: PlanetType,
         *,
         num_slots: int,
     ) -> None:
+        cluster, planet_symbol = _decompose_system_id(system_id)
+        if planet_symbol is None:
+            raise ValueError(f"SystemID {system_id} does not decompose to a planet symbol.")
+
         self.cluster = cluster
         self.symbol = planet_symbol
         self.type = planet_type
         self.num_slots = num_slots
+        self.adjacencies = set()
 
         self.pieces = set()
-        self.adjacencies = []
 
     # def add_piece
     # def remove_piece
