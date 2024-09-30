fc24858ab5c6a39687a039067f53884a8170fba8
emptyset <emptierset@gmail.com>
2024-09-30 00:05:08 -0700

    Fix ambition marker value bugs

--- arcsync/ambition.py
+++ arcsync/ambition.py
@@ -1,3 +1,4 @@
+import dataclasses
 import enum
 import typing
 from collections.abc import Mapping, MutableSet
@@ -23,25 +24,39 @@ class Ambition(enum.Enum):
 Power = NewType("Power", int)
 
 
+@dataclasses.dataclass(kw_only=True, frozen=True)
+class AmbitionMarkerFace(object):
+    first_place: Power
+    second_place: Power
+
+
 # TODO(base): We may need to keep track of whether there was an ambition declared this round, for
 # things like Galactic Bards. That may get tracked here, or somewhere else.
 @typing.final
 class AmbitionMarker(DunderDictReprMixin):
-    value: Power
+    first_place: Power
+    second_place: Power
 
-    _front: Final[Power]
-    _back: Final[Power]
+    _front: Final[AmbitionMarkerFace]
+    _back: Final[AmbitionMarkerFace]
 
-    def __init__(self, *, front: Power, back: Power, starts_on_back: bool = False):
-        self.value = back if starts_on_back else front
+    def __init__(
+        self, *, front: AmbitionMarkerFace, back: AmbitionMarkerFace, starts_on_back: bool = False
+    ):
+        self.first_place = back.first_place if starts_on_back else front.first_place
+        self.second_place = back.second_place if starts_on_back else front.second_place
         self._front = front
         self._back = back
 
     def flip(self) -> None:
-        if self.value == self._front:
-            self.value = self._back
+        if self.first_place == self._front.first_place:
+            self.first_place = self._back.first_place
         else:
-            self.value = self._front
+            self.first_place = self._front.first_place
+        if self.second_place == self._front.second_place:
+            self.second_place = self._back.second_place
+        else:
+            self.second_place = self._front.second_place
 
 
 @typing.final
@@ -55,10 +70,19 @@ class AmbitionManager(DunderDictReprMixin):
         super().__init__()
         self.available_markers = set(
             [
-                AmbitionMarker(front=Power(5), back=Power(9)),
-                # TODO(base): How many points is the back? 2?
-                AmbitionMarker(front=Power(3), back=Power(100)),
-                AmbitionMarker(front=Power(2), back=Power(0)),
+                # TODO(base): Double check all of these numbers.
+                AmbitionMarker(
+                    front=AmbitionMarkerFace(first_place=Power(5), second_place=Power(100)),
+                    back=AmbitionMarkerFace(first_place=Power(9), second_place=Power(100)),
+                ),
+                AmbitionMarker(
+                    front=AmbitionMarkerFace(first_place=Power(3), second_place=Power(100)),
+                    back=AmbitionMarkerFace(first_place=Power(100), second_place=Power(100)),
+                ),
+                AmbitionMarker(
+                    front=AmbitionMarkerFace(first_place=Power(2), second_place=Power(0)),
+                    back=AmbitionMarkerFace(first_place=Power(100), second_place=Power(100)),
+                ),
             ]
         )
         self.boxes = {
@@ -75,20 +99,26 @@ class AmbitionManager(DunderDictReprMixin):
         if event_bus is None:
             event_bus = EventBus()
         self._event_bus = event_bus
-        self.initialize_event_handlers()
 
-    def initialize_event_handlers(self) -> None:
-        pass
+        def handle_ambition_declared_event(event: AmbitionDeclaredEvent) -> None:
+            self.declare(event.player, event.ambition)
+
+        self._event_bus.subscribe(AmbitionDeclaredEvent, handle_ambition_declared_event)
 
     def declare(self, player: Color, ambition: Ambition) -> None:
         if not self.available_markers:
             raise ValueError("Cannot declare ambition without available ambition marker.")
         am: AmbitionMarker = self._pop_most_valuable_available_marker()
         self.boxes[ambition].add(am)
-        self._event_bus.publish(AmbitionDeclaredEvent(player, ambition))
+        # TODO(base): We need to use a different type of event for declaring an ambition, and
+        # publishing that an ambition was declared. Using the same event causes recursion.
+        # self._event_bus.publish(AmbitionDeclaredEvent(player, ambition))
 
-    def value_of(self, ambition: Ambition) -> Power:
-        return Power(sum(marker.value for marker in self.boxes[ambition]))
+    def first_place_value(self, ambition: Ambition) -> Power:
+        return Power(sum(marker.first_place for marker in self.boxes[ambition]))
+
+    def second_place_value(self, ambition: Ambition) -> Power:
+        return Power(sum(marker.second_place for marker in self.boxes[ambition]))
 
     def clear(self) -> None:
         for markers in self.boxes.values():
@@ -96,10 +126,10 @@ class AmbitionManager(DunderDictReprMixin):
             markers.clear()
 
     @property
-    def most_valuable_available_marker(self) -> AmbitionMarker | None:
+    def most_valuable_available_marker(self) -> AmbitionMarker:
         if not self.available_markers:
-            return None
-        return max(self.available_markers, key=lambda m: m.value)
+            raise ValueError("There is no most valuable ambition marker when none is available.")
+        return max(self.available_markers, key=lambda m: m.first_place)
 
     def _pop_most_valuable_available_marker(self) -> AmbitionMarker:
         am: AmbitionMarker | None = self.most_valuable_available_marker
